e24f5e7758cd28b4e9bfbafa84ebd72aaf743738
JoseLuisColcha <jose.colcha@epn.edu.ec>
2022-05-27 01:34:06 -0500

    autenticacion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'app2022a-df0bf',
+    appId: '1:404178950445:web:e1ec9c78ba272025f01ba4',
+    storageBucket: 'app2022a-df0bf.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBh8esKZq8RS5bZ38pD0AC0c-DLkspAaZg',
+    authDomain: 'app2022a-df0bf.firebaseapp.com',
+    messagingSenderId: '404178950445',
+    measurementId: 'G-GZG8D0W336',
+  },
   production: true
 };
